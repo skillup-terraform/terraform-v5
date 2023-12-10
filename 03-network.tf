0c005cdc76c940496499aafd99d31d536cf9179a
“suresh.akidev” <sureshr.tamizan@gmail.com>
2023-12-11 00:34:22 +0530

    updated

--- 03-network.tf
+++ 03-network.tf
@@ -20,8 +20,8 @@ resource "azurerm_virtual_network" "terra_vm_network" {
 
 resource "azurerm_network_interface" "name" {
   name = var.network_interface_name
-    location             = var.location
-  resource_group_name  = var.resource_group_name
+    location             = azurerm_resource_group.terra_rg.location
+  resource_group_name  = azurerm_resource_group.terra_rg.name
   ip_configuration {
     name = "interal"
     subnet_id = azurerm_subnet.name.id
