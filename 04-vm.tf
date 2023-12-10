81ac5cf4a4c2e1ac067c7edecbf529813ab0a3ce
“suresh.akidev” <sureshr.tamizan@gmail.com>
2023-12-10 19:29:22 +0530

    updated

--- 04-vm.tf
+++ 04-vm.tf
@@ -1,25 +1,24 @@
-resource "azurerm_linux_virtual_machine" "terra-linux-vm-01" {
-  resource_group_name = azurerm_resource_group.terra_rg.name
-  name = var.vm_name
-  location = var.location
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  size = "Standard_D2s_v3"
-  network_interface_ids = [azurerm_network_interface.name.id]
-  admin_username = "azureuser"
-  disable_password_authentication = true
+resource "azurerm_windows_virtual_machine" "windows_vm" {
+  name                = "example-machine"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id
+  ]
+
   os_disk {
-    caching = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
-  admin_ssh_key {
-  username   = "azureuser"
-  public_key = file("~/.ssh/suresh.akidev.pub")
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
   }
 }
 
